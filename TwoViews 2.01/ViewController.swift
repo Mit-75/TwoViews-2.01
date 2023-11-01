dadc4a776d6fd08b77626f376a4effa615c8e220
Dmitry Parkhomenko <oku_74@mail.ru>
2023-11-01 23:01:06 +0500

    Initial Commit

---
+++ TwoViews 2.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViews 2.01
+//
+//  Created by Dmitry Parhomenko on 01.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
